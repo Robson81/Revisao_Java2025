a63c94ddaf4541f6f6e8c37e6388a96e92756502
Robson Barbosa <robson.barbosa12@gmail.com>
2025-10-27 16:54:30 -0300

    primeiro projeto JAVA - Revisão

---
+++ Main.java
@@ -0,0 +1,19 @@
+package curso_java_completo;
+
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		double x = 10.35784;
+		System.out.println();
+		System.out.print("Olá Mundo!");
+		System.out.println("Bom dia");
+		System.out.printf("%.2f\n", x);
+		System.out.printf("%.4f\n", x);
+
+	}
+
+}
